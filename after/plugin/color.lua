c3fcd893d2d5acacf12058264d408ded280e0fbb
Srivarshan Selvaraj <srivarshan02@gmail.com>
2022-12-20 15:06:19 +0530

    Minor changes

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,5 +1,5 @@
 function ApplyColor(color) 
-	color = color or "rose-pine"
+	color = color or "gruvbox-material"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
