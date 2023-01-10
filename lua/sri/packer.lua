60d87759505a275348f3f10bfb35a6cb7e101e06
Srivarshan <srivarshan02@gmail.com>
2023-01-10 20:56:29 +0530

    Minor formatting

--- lua/sri/packer.lua
+++ lua/sri/packer.lua
@@ -5,61 +5,61 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Telescope (fuzzy file finder)
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Telescope (fuzzy file finder)
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	-- Monokai color scheme
-	use({
-		'tanvirtin/monokai.nvim',
-		as = 'monokai',
-	})
+    -- Monokai color scheme
+    use({
+        'tanvirtin/monokai.nvim',
+        as = 'monokai',
+    })
 
-	-- Rose-pine color scheme
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-	})
+    -- Rose-pine color scheme
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+    })
 
-	-- Gruvbox color scheme
-	use 'sainnhe/gruvbox-material'
+    -- Gruvbox color scheme
+    use 'sainnhe/gruvbox-material'
 
-	-- Treesitter
-	use ( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    -- Treesitter
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-	-- Undo tree
-	use 'mbbill/undotree'
+    -- Undo tree
+    use 'mbbill/undotree'
 
-	-- Auto commenter
-	use 'numToStr/Comment.nvim'
+    -- Auto commenter
+    use 'numToStr/Comment.nvim'
 
-	-- Auto brackets
-	use "windwp/nvim-autopairs"
+    -- Auto brackets
+    use "windwp/nvim-autopairs"
 
-	-- LSP
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
 end)
