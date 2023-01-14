82650ab34df55da088dde38fe9d968acab351ce8
Srivarshan <srivarshan02@gmail.com>
2023-01-14 12:00:50 +0530

    Add a few keymaps

--- lua/sri/remap.lua
+++ lua/sri/remap.lua
@@ -3,3 +3,11 @@ vim.g.mapleader = " "
 
 -- Set space+e to open netrw (the file explorer)
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Set space+y to copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Paste without copying highlighted region
+vim.keymap.set("x", "<leader>p", "\"_dP")
