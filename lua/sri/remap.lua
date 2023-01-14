ecfa1736a5125b53347896b5db1eb165a1affe72
Srivarshan <srivarshan02@gmail.com>
2023-01-14 11:40:39 +0530

    Remove purposeless set function

--- lua/sri/remap.lua
+++ lua/sri/remap.lua
@@ -1,5 +1,2 @@
--- Set leader key (space)
-vim.g.mapleader = " "
-
--- Set space+p+v to open netrw (the file explorer)
+-- Set space+e to open netrw (the file explorer)
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
