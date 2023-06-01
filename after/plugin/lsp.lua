cdb028bdb3cd367507314d193006abcd2aa92fda
Srivarshan <srivarshan02@gmail.com>
2023-06-01 21:23:36 +0530

    Update completion keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,6 +9,27 @@ lsp.ensure_installed({
     'rust_analyzer',
 })
 
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+local cmp = require('cmp')
+
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
 lsp.set_preferences({
     suggest_lsp_servers = true,
     sign_icons = {
@@ -52,3 +73,7 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
